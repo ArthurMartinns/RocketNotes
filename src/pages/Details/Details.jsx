e77b24a1e3942ae7f0173d5001998de02c410aca
arthurmartinsscandido <arthurmartinsscandido@gmail.com>
2024-07-29 13:35:16 -0300

    Configurações iniciais.

---
+++ src/pages/Details/Details.jsx
@@ -0,0 +1,12 @@
+import { Container } from './styles.js'
+
+export default function Details() {
+
+  return (
+    <Container>
+      <h1>
+        Hello World
+      </h1>
+    </Container>
+  )
+}
